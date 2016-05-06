6d01427bd234a4bce10756be8d8cd16c2b9d6058
xavier <xavier.lugol@laposte.net>
2016-05-06 12:01:55 +0200

    First commit

---
+++ src/ExoGit2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ExoGit2 {
+	private int prevint1 = 1;
+	private int prevint2 = 1;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
